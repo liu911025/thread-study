7e398b422ce1b86b2ae9b0c517d0cd2ea2a812ff
ljt <liujiatai2016@163.com>
2019-03-29 18:02:12 +0800

    dddd

---
+++ src/main/java/com/example/demotest/thread/threadLocal/SynchronizedDemo.java
@@ -0,0 +1,11 @@
+package com.example.demotest.thread.threadLocal;
+
+public class SynchronizedDemo {
+
+    public static void main(String[] args) {
+        test();
+    }
+
+    public static synchronized void test(){
+    }
+}
