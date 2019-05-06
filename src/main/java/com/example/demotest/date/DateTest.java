021b010462f8febcf6f32988fd94d8af720e177b
ljt <liujiatai2016@163.com>
2019-05-06 15:13:55 +0800

    时间工具类

---
+++ src/main/java/com/example/demotest/date/DateTest.java
@@ -0,0 +1,30 @@
+package com.example.demotest.date;
+
+import com.example.demotest.util.DateUtils;
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateTest {
+
+    private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    @Test
+    public void test_01() throws ParseException {
+        String str = "2019-05-06 14:25:03";
+        Date date = sdf.parse(str);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.MINUTE, 10);
+
+        System.out.println(sdf.format(calendar.getTime()));
+
+        int i = DateUtils.dateCompare(calendar.getTime(), new Date());
+        System.out.println(i);
+    }
+
+}
--- src/main/java/com/example/demotest/service/impl/TestServiceImpl.java
+++ src/main/java/com/example/demotest/service/impl/TestServiceImpl.java
@@ -15,17 +15,17 @@ public class TestServiceImpl implements TestService {
     }
 
     @Override
-    @HandlingTime
+    //@HandlingTime
     public String test01(String q) {
         try {
             if ("q".equals(q)) {
                 //throw new NullPointerException("eqwqwq");
                 int a = 1 / 0;
             }
-        }catch (ArithmeticException e) {
+        }/*catch (ArithmeticException e) {
             log.info("test01异常, 异常信息为: {}" + e.getMessage(), e);
             throw new ArithmeticException(e.getMessage());
-        }
+        }*/
         catch (Exception e) {
             log.info("test01异常, 异常信息为: {}" + e.getMessage(), e);
             throw new NullPointerException(e.getMessage());
